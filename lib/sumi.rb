4f61145ae4135894d73a6540ffa5b3f0b7fa7bb4
Joel Drapper <joel@drapper.me>
2025-01-31 14:05:07 +0000

    Require set

--- lib/sumi.rb
+++ lib/sumi.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "sumi/version"
+require "set"
 
 module Sumi
 	def self.inspect(object, indent: 0, tab_width: 2, max_width: 60, max_depth: 5, max_instance_variables: 10, original_object: nil)
