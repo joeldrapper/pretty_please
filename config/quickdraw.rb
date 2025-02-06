321936e42c900d42ecdaf607e335867010c7ccf4
Marco Roth <marco.roth@intergga.ch>
2025-02-06 21:37:29 +0100

    RuboCop

--- config/quickdraw.rb
+++ config/quickdraw.rb
@@ -23,34 +23,34 @@ class Example
 end
 
 module ActiveRecord
-  if defined?(ActiveRecord::Base)
-    raise "ActiveRecord::Base is already defined, remove the 'ActiveRecord::Base' stub in the 'config/quickdraw.rb' file."
-  end
+	if defined?(ActiveRecord::Base)
+		raise "ActiveRecord::Base is already defined, remove the 'ActiveRecord::Base' stub in the 'config/quickdraw.rb' file."
+	end
 
-  class Base
-  end
+	class Base
+	end
 end
 
 class TestModel < ActiveRecord::Base
-  attr_reader :attributes
+	attr_reader :attributes
 
-  def initialize
-    @attributes = {
-      "id" => 1,
-      "name" => "Test Model",
-      "created_at" => Time.new("2025-02-06 01:02:03 UTC"),
-      "updated_at" => Time.new("2025-02-06 04:05:06 UTC"),
-      "date" => Date.parse("2025-02-06"),
-      "tags" => ["tag_1", "tag_2", "tag_3"],
-      "attribute_1" => "value_1",
-      "attribute_2" => "value_2",
-      "attribute_3" => "value_3",
-      "attribute_4" => "value_4",
-      "attribute_5" => "value_5",
-      "attribute_6" => "value_6",
-      "attribute_7" => "value_7",
-      "attribute_8" => "value_8",
-      "attribute_9" => "value_9",
-    }
-  end
+	def initialize
+		@attributes = {
+			"id" => 1,
+			"name" => "Test Model",
+			"created_at" => Time.new("2025-02-06 01:02:03 UTC"),
+			"updated_at" => Time.new("2025-02-06 04:05:06 UTC"),
+			"date" => Date.parse("2025-02-06"),
+			"tags" => ["tag_1", "tag_2", "tag_3"],
+			"attribute_1" => "value_1",
+			"attribute_2" => "value_2",
+			"attribute_3" => "value_3",
+			"attribute_4" => "value_4",
+			"attribute_5" => "value_5",
+			"attribute_6" => "value_6",
+			"attribute_7" => "value_7",
+			"attribute_8" => "value_8",
+			"attribute_9" => "value_9",
+		}
+	end
 end
